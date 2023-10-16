3c960e0498bb9f8d84a581540952cf123f263d7b
tech-n1c <nicimaiwald@gmail.com>
2023-10-16 11:26:35 +0200

    Initialized Spring Backend

---
+++ backend2shine/src/main/java/com/backend2shine/Backend2shineApplication.java
@@ -0,0 +1,13 @@
+package com.backend2shine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend2shineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backend2shineApplication.class, args);
+	}
+
+}
---
+++ backend2shine/src/test/java/com/backend2shine/Backend2shineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend2shine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend2shineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
