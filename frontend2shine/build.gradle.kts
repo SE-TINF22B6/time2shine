d4417d97e516d31cccc4899bc6c3ff293b89b0b6
sfrancisw <sfrancisw@gmail.com>
2023-10-17 11:39:16 +0200

    dummy java frontend website

--- frontend2shine/build.gradle.kts
+++ frontend2shine/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("application")
 }
 
+
+
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
@@ -13,6 +15,11 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.sparkjava:spark-core:2.9.3")
+}
+
+application {
+    this.mainClass = "org.example.MainFile"
 }
 
 tasks.test {
---
+++ frontend2shine/src/main/java/org/example/MainFile.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import static spark.Spark.*;
+
+public class MainFile {
+    public static void main(String[] args) {
+        port(8080);
+
+        get("/", (req, res) -> "<h2>Hello, this is a simple dummy web application!</h2>");
+    }
+}
