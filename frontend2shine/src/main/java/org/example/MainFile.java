913fdea06003b1dbb19c88ddb36dcee89715d114
sfrancisw <sfrancisw@gmail.com>
2023-10-17 11:40:31 +0200

    dummy java frontend website

--- frontend2shine/src/main/java/org/example/MainFile.java
+++ frontend2shine/src/main/java/org/example/MainFile.java
@@ -4,6 +4,7 @@ import static spark.Spark.*;
 
 public class MainFile {
     public static void main(String[] args) {
+        System.out.println("Test");
         port(8080);
 
         get("/", (req, res) -> "<h2>Hello, this is a simple dummy web application!</h2>");
