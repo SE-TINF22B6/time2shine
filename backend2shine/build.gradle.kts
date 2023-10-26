e860650adf9a4e43424a30d029e84e159c5a062b
tech-n1c <nicimaiwald@gmail.com>
2023-10-26 13:09:59 +0200

    Updating Database configuration

--- backend2shine/build.gradle.kts
+++ backend2shine/build.gradle.kts
@@ -18,10 +18,9 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-//	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-//	runtimeOnly("org.postgresql:postgresql")
+	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
