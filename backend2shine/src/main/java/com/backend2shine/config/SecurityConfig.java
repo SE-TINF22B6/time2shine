5fceb5a51a69b900846cd011782c5ada53af67a8
tech-n1c <nicimaiwald@gmail.com>
2023-10-26 10:25:12 +0200

    Updated Gradle

--- backend2shine/src/main/java/com/backend2shine/config/SecurityConfig.java
+++ backend2shine/src/main/java/com/backend2shine/config/SecurityConfig.java
@@ -8,20 +8,20 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfig {
-
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/").permitAll();
-                    auth.anyRequest().authenticated();
-                } )
-                .oauth2Login(withDefaults())
-                .formLogin(withDefaults())
-                .build();
-    }
+//
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .authorizeHttpRequests(auth -> {
+//                    auth.requestMatchers("/", "/index.html").permitAll();
+//                    auth.anyRequest().authenticated();
+//                } )
+//                .oauth2Login(withDefaults())
+//                .formLogin(withDefaults())
+//                .build();
+//    }
 
 }
