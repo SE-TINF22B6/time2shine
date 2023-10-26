0ec2b9ee5ae9d9a5a8ddc05e511c0a4f2ec3fc5b
tech-n1c <nicimaiwald@gmail.com>
2023-10-26 10:25:29 +0200

    Updated Gradle

--- backend2shine/src/main/java/com/backend2shine/config/SecurityConfig.java
+++ backend2shine/src/main/java/com/backend2shine/config/SecurityConfig.java
@@ -2,11 +2,11 @@ package com.backend2shine.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import static org.springframework.security.config.Customizer.withDefaults;
 
 //@Configuration
 //@EnableWebSecurity
