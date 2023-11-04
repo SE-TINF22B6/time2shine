0bf0edfe0a52cd1fa07456bccc5cc8c55183ffbf
tech-n1c <nicimaiwald@gmail.com>
2023-11-04 18:21:10 +0100

    Implementing Account JUnit Tests

--- backend2shine/src/test/java/com/backend2shine/Backend2shineApplicationTests.java
+++ backend2shine/src/test/java/com/backend2shine/Backend2shineApplicationTests.java
@@ -1,14 +1,20 @@
 package com.backend2shine;
 
+import com.backend2shine.accounts.Account;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.backend2shine.accounts.Account;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 @SpringBootTest
 class Backend2shineApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 
 }
