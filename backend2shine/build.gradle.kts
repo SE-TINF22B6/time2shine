5ebeaae49079488b98d11e19855c12dcb2e8d138
tech-n1c <nicimaiwald@gmail.com>
2023-11-25 13:58:25 +0100

    Basic Keycloak Setup

--- backend2shine/build.gradle.kts
+++ backend2shine/build.gradle.kts
@@ -18,6 +18,9 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+//	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("org.postgresql:postgresql")
--- backend2shine/src/main/java/com/backend2shine/Backend2shineApplication.java
+++ backend2shine/src/main/java/com/backend2shine/Backend2shineApplication.java
@@ -3,11 +3,18 @@ package com.backend2shine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class Backend2shineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Backend2shineApplication.class, args);
+
+
+//		SpringApplication app =  new SpringApplication(Backend2shineApplication.class);
+//
+//		app.run(args);
 	}
 
 }
