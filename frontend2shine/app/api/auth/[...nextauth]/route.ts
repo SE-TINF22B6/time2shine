da29e39cd5f2b42fb65b7856d3af3d6ebe3e1309
tech-n1c <nicimaiwald@gmail.com>
2024-04-11 14:22:02 +0200

    Site setup

--- frontend2shine/app/api/auth/[...nextauth]/route.ts
+++ frontend2shine/app/api/auth/[...nextauth]/route.ts
@@ -20,6 +20,13 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
+  pages: {
+    signIn: '/login',
+    signOut: '/',
+    error: '/', // Error code passed in query string as ?error=
+    verifyRequest: '', // (used for check email message)
+    // newUser: '/' // New users will be directed here on first sign in (leave the property out if not of interest)
+  },
   secret: process.env.NEXTAUTH_SECRET as string,
 };
 
