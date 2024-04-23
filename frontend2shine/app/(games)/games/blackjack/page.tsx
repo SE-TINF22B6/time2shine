054df3306a76c836f892bd4af4a1a510f181c5de
Leonmandreesen <leonandreesen@gmx.de>
2024-04-23 10:11:41 +0200

    Added BJ page

---
+++ frontend2shine/app/(games)/games/blackjack/page.tsx
@@ -0,0 +1,33 @@
+export const metadata = {
+  title: 'BlackJack Wiki - time2shine',
+  description: 'Online Gaming Platform',
+}
+
+export default function Games() {
+  return (
+    <section className="relative">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+            <h1 className="h1 mb-4" data-aos="fade-up">BlackJack.</h1>
+            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">An overview of our current
+              and upcoming games.</p>
+            <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
+              <div data-aos="fade-up" data-aos-delay="400">
+                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0"
+                   href="/blackjack">Play Black Jack</a>
+              </div>
+            </div>
+          </div>
+
+
+
+          bla fasel blubber
+
+        </div>
+      </div>
+    </section>
+  )
+}
