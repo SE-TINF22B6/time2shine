e146dc73c3635d4582ecc576c5d3a32c4226630a
nicol <nicimaiwald@gmail.com>
2024-04-24 17:53:30 +0200

    Reverting stuff

--- frontend2shine/app/(auth)/profile/page.tsx
+++ frontend2shine/app/(auth)/profile/page.tsx
@@ -1,7 +1,9 @@
-'use client';
+// Metadata for the page
+export const metadata = {
+    title: 'My Profile - time2shine', // Page title
+    description: 'Online Gaming Platform', // Page description
+}
 
-import React, { useEffect, useState } from 'react';
-import { Helmet } from 'react-helmet';
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 
@@ -10,28 +12,9 @@ export default async function Profile() {
     const session = await getServerSession(authOptions); // Get the session from the server
     const user = session?.user; // Get the user from the session
 
-    type Highscores = { [key: string]: number };
-    const [highscores, setHighscores] = useState<Highscores>({});
-
-    useEffect(() => {
-        if (user) {
-            // Fetch highscores for each game for the logged-in user
-            ['blackjack', 'poker'].forEach(game => {
-                fetch(`https://api.maiwald.cc/highscores?username=${user.name}&game=${game}`)
-                    .then(response => response.json())
-                    .then(data => setHighscores(prevScores => ({...prevScores, [game]: data})));
-            });
-        }
-    }, [user]);
-
     return (
         <>
             <section className="bg-ct-blue-600  min-h-screen pt-20">
-                <Helmet>
-                    <title>Stats - time2shine</title>
-                    <meta name="description" content="Online Gaming Platform" />
-                </Helmet>
-
                 <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center">
                     <div>
                         <p className="mb-3 text-5xl text-center font-semibold">
@@ -53,11 +36,6 @@ export default async function Profile() {
                                 <div className="mt-8">
                                     <p className="mb-3">Name: {user.name}</p>
                                     <p className="mb-3">Email: {user.email}</p>
-                                    {/* New code to display highscores */}
-                                    <p className="mb-3">Your Highscores:</p>
-                                    {Object.entries(highscores).map(([game, score]) => (
-                                        <p key={game}>{game}: {score}</p>
-                                    ))}
                                 </div>
                             </div>
                         )}
