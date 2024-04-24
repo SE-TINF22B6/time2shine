6ea23500ab714edea42c8492e935d0d7694d0533
tech-n1c <nicimaiwald@gmail.com>
2024-04-24 10:00:19 +0200

    Changed routing

--- frontend2shine/app/(stats)/stats/page.tsx
+++ frontend2shine/app/(stats)/stats/page.tsx
@@ -1,30 +1,63 @@
-export const metadata = {
-  title: 'Stats - time2shine',
-  description: 'Online Gaming Platform',
-}
+'use client';
+// export const metadata = {
+//   title: 'Black Jack stats - time2shine',
+//   description: 'Online Gaming Platform',
+// }
+
+import React, { useEffect, useState } from 'react';
+
+type DataItem = {
+  username: string;
+  score: number;
+};
 
 export default function Stats() {
-  return (
-    <section className="relative">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+  const [data, setData] = useState<DataItem[]>([]);
 
-          {/* Section header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h1 className="h1 mb-4" data-aos="fade-up">time2shine stats & leaderboards.</h1>
-            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Here you can compare your results with friends and other players.</p>
-            <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
-              <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="/stats/blackjack">Black
-                  Jack stats</a>
+  useEffect(() => {
+    fetch('https://api.maiwald.cc/highscores/games/blackjack')
+        .then(response => response.json())
+        .then(data => setData(data));
+  }, []);
+
+  return (
+      <section className="relative">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+          <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+
+            {/* Page header */}
+            <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+              <h1 className="h1">BlackJack statistics.</h1>
+            </div>
+            <div className="flex flex-col">
+              <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                  <div className="overflow-hidden">
+                    <table className="min-w-full text-left text-sm font-light text-surface dark:text-white">
+                      <thead className="border-b border-neutral-200 font-medium dark:border-white/10">
+                      <tr>
+                        <th scope="col" className="px-6 py-4">Rank</th>
+                        <th scope="col" className="px-6 py-4">Player</th>
+                        <th scope="col" className="px-6 py-4">Score</th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                      {data.map((item, index) => (
+                          <tr key={index} className="border-b border-neutral-200 transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-white/10 dark:hover:bg-neutral-600">
+                            <td className="whitespace-nowrap px-6 py-4 font-medium">{index + 1}</td>
+                            <td className="whitespace-nowrap px-6 py-4">{item.username}</td>
+                            <td className="whitespace-nowrap px-6 py-4">{item.score}</td>
+                          </tr>
+                      ))}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
               </div>
             </div>
+
           </div>
-
-          bla fasel blubber
-
         </div>
-      </div>
-    </section>
+      </section>
   )
 }
