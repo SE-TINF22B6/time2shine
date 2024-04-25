e1cfe7f2cf3814b15b5604f460ef0e1f7676ac04
tech-n1c <nicimaiwald@gmail.com>
2024-04-25 14:08:21 +0200

    Making shit work

--- frontend2shine/components/hero.tsx
+++ frontend2shine/components/hero.tsx
@@ -1,7 +1,29 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { getSession } from "next-auth/react";
 import VideoThumb from '@/public/images/trailer-thumbnail.png'
 import ModalVideo from '@/components/modal-video'
 
+
 export default function Hero() {
+  const [playNowUrl, setPlayNowUrl] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const session = await getSession();
+      const user = session?.user;
+
+      // Prepare the URL with user details as query parameters
+      const url = `https://engine.maiwald.cc?username=${encodeURIComponent(user?.name || '')}&email=${encodeURIComponent(user?.email || '')}`;
+
+      setPlayNowUrl(url);
+    };
+
+    fetchData();
+  }, []);
+
+
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6 relative">
@@ -28,7 +50,8 @@ export default function Hero() {
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Our first playable game is Black Jack - with more fun games coming up. Stay tuned!</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#">Play now!</a>
+                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0"
+                   href={playNowUrl}>Play now!</a>
               </div>
               <div data-aos="fade-up" data-aos-delay="600">
                 <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="/games">Learn more</a>
